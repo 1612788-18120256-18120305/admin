4913a226f71b331f020664cd03478706e821e2f6
Le Quoc Dat <18120305@student.hcmus.edu.vn>
2021-12-29 21:40:14 +0700

    login and create admin

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import history from './history';
-import Layout from './containers/Layout';
 import { Router, Switch, Route } from 'react-router-dom';
 
+const Layout = lazy(() => import('./containers/Layout'));
+const Login = lazy(() => import('./pages/Login'));
 function App() {
   return (
     <Router history={history}>
       <Switch>
+        <Route path="/login" exact component={Login} />
         <Route path="/" component={Layout} />
       </Switch>
     </Router>
--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import ReactDOM from 'react-dom';
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Provider } from 'react-redux';
 import { Windmill } from '@windmill/react-ui';
+import ThemedSuspense from './components/ThemedSuspense';
 import store from './myStore';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
   <Provider store={store}>
-    <Windmill>
-      <App />
-    </Windmill>
+    <Suspense fallback={<ThemedSuspense />}>
+      <Windmill>
+        <App />
+      </Windmill>
+    </Suspense>
   </Provider>,
   document.getElementById('root')
 );
